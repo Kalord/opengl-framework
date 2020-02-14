5f77b660e224eef96212a012ecea899ddcb60c9f
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-02-14 23:52:29 +0800

    Added method: init

---
+++ core/Init.cpp
@@ -0,0 +1,15 @@
+#include "Init.hpp"
+
+/**
+ * Setup Open GL
+ * Set major and minor version
+ */
+void Init::init(int major, int minor)
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+}
--- core/Init.hpp
+++ core/Init.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "OpenGL.hpp"
+
 /**
  * Init OpenGL
  */
