c62f6da84977dc0ac7b34fb4cb3d51f0e829fa95
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-02-14 23:44:22 +0800

    Refactoring

---
+++ core/Init.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * Данный класс отвечает за настройку OpenGL
+ */
+class Init
+{
+public:
+	/**
+	 * Данный класс инициализирует OpenGL
+	 */
+	static void init(int major, int minor);
+};
